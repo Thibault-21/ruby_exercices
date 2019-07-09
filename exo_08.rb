8fa4971b2c0c64b81dfd6e22a49c491e29568f94
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-09 10:46:33 +0200

    udpate

--- exo_08.rb
+++ exo_08.rb
@@ -1,5 +1,11 @@
+class User 
+    attr_accessor :first_name, :last_name
+end
 
+user = User.new
+user.first_name = "bob"
+puts user.first_name
 #demande le nom de l'utilisateur dans la console / terminal
-user_name = gets.chomp
-puts user_name
-puts Bonjour user-name
+# user_name = gets.chomp
+# puts user_name
+# puts Bonjour 
