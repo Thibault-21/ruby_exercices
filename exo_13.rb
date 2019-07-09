9656db9bdc336d076b15b36c552e95ccdf9ba747
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-09 14:18:02 +0200

    update

---
+++ exo_13.rb
@@ -0,0 +1,5 @@
+puts "Hello there ! what is your birth' year? ? (only number)?"
+birth_year= gets.to_i
+
+#qui demande son année de naissance à l'utilisateur, 
+# puis qui va ressortir chaque année depuis son année de naissance jusqu'à 2018.
