10714c78f0b99edc2adb41772fa50810ea80eee3
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-09 10:59:39 +0200

    update

--- exo_08.rb
+++ exo_08.rb
@@ -1,15 +1,16 @@
 puts "Hello user, what's your user-name ?"
 
 class User 
-    attr_accessor :user_name,
+    attr_accessor :name,
 end
 
-user_name = User.new
-# user.user_name = "bob"
+my_user = User.new
+my_user.name = "bob"
+puts my_user.name 
 # puts "My name is " + "" + user.first_name
 #demande le nom de l'utilisateur dans la console / terminal
-user_name = gets.chomp
-puts user_name
-end 
-puts "Bonjour " + user-name 
-end
+# user_name = gets.chomp
+# puts user_name
+# end 
+# puts "Bonjour " + user-name 
+# end
