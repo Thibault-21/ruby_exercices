f2225b8dddcc2213b42dfc28d880cb65b3836ba0
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-09 11:29:16 +0200

    update

---
+++ exo_09.rb
@@ -0,0 +1,6 @@
+puts "Hello there, whats your first name ?"
+first_name = gets.chomp
+puts "Hi #{first_name}, what is your last name ?"
+last_name = gets.chomp
+
+puts "Hi #{first_name, last-name}, welcome back!"
