bb26ebc3a6f7e10cf2da4c0d77d32ba055ed5b1e
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-09 17:33:31 +0200

    update

--- exo_18.rb
+++ exo_18.rb
@@ -1,2 +1,16 @@
-mail ["jean.dupont.01@email.fr"] 50.times do |i|
-    prints email  
+prenom = "jean"
+nom ="dupont"
+tab_email = Array.new
+
+50.times do |i| 
+    tab_email << prenom + "." + nom + (i+1).to_s + "@email.fr"
+
+end
+    puts tab_email
+
+# i.to_s 
+# string = chaine de caractères 
+# i est un entier pour l'utiliser dans une chaine de caractère il faut le convertir avec i.to_s (cad, i convert to s: s pour string)
+#     # Écris un programme exo_18.rb qui va créer une liste de 50 faux emails 
+#     # et les stocker dans une array. 
+#     # Voici le format que devront avoir les faux emails :
