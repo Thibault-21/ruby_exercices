94aeb8f41aa389d66aae2f56bbdee220f1bf08cf
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-09 09:39:22 +0200

    update

--- exo_05.rb
+++ exo_05.rb
@@ -1,11 +1,11 @@
 puts "On va compter le nombre d'heures de travail à THP"
-#le code va intégrer dans la chaine, l'objet travail et ses variables 
+#le code va intégrer dans la chaine, l'objet "travail" et ses variables qui vont constituer la somme des heures de travail
 puts "Travail : #{10 * 5 * 11}" 
-#le code va intégrer dans la chaine, l'objet en minutes ça fait et ses variables
+#le code va intégrer dans la chaine, l'objet "en minutes ça fait" et ses variables
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
 
 puts "Et en secondes ?"
-#intégrent les variables Et en secondes dans la chaine
+#intégrent les variables "Et en secondes" dans la chaine pour avoir le résultat en secondes
 puts 10 * 5 * 11 * 60 * 60
 
 #le code demande à vérifier si l'inégalité est vraie 
@@ -16,16 +16,16 @@ puts 3 + 2 < 5 - 7
 puts "Ça fait combien 3 + 2 ? #{3 + 2}"
 #le code demande combien fait l'objet ajouté à la chaine #{5-7}
 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
-#le code décalre que c'est faux en bout de chaine
+#le code déclare que c'est faux 
 puts "Ok, c'est faux alors !"
 
 puts "C'est drôle ça, faisons-en plus :"
 
-#le code demande si 5 est plus grand que -2
+#le code demande si 5 est plus grand que -2 et déclare que c'est vrai
 puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
 
-# le code demande 5 est > ou = à -2
+# le code demande 5 est > ou = à -2 et déclare que c'est vrai 
 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
 
-# le code demande 5 si est < ou = à -2
+# le code demande 5 si est < ou = à -2 déclare que c'est faux
 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
