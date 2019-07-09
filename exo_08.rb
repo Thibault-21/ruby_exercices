2a6d990745b9b0d19dae8296ab034f08bc861305
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-09 10:49:10 +0200

    update

--- exo_08.rb
+++ exo_08.rb
@@ -6,7 +6,7 @@ end
 
 user = User.new
 user.first_name = "bob"
-puts user.first_name
+puts "My name is" + user.first_name
 #demande le nom de l'utilisateur dans la console / terminal
 # user_name = gets.chomp
 # puts user_name
