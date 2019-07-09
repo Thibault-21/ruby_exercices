88eed6b3946dc0eb83e4e724b9d8c042a4d91aeb
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-09 15:56:07 +0200

    update

---
+++ exo_16.rb
@@ -0,0 +1,6 @@
+puts "Hello there ! what is your birth' year? ? (only number)?"
+user_year = gets.chomp.to_i
+
+(2018 - user_year +1).times do|i|
+    puts "#{user_year + i} " + " #{2018 - user_year - i} years ago you were #{i}" 
+end
